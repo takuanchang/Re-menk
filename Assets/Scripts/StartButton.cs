4f151fd564ca39fb477cc0dfdf987cc75b12189c
saikiRA <sumou1011.2@gmail.com>
2023-10-30 01:16:27 +0900

    feature: スタートボタンを押すとシーン遷移するように実装

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
-public class EventHandler : MonoBehaviour
+public class StartButton : MonoBehaviour
 {
+    public void ChangeToGameScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
