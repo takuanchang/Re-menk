e7bbcef079cae2b581b2bc182c689276fc44e26e
saikiRA <sumou1011.2@gmail.com>
2023-11-02 22:43:44 +0900

    update: TitlePieceを削除し、Piece Prefabをタイトル画面に用いるように変更

--- Assets/Scripts/TitlePieceAnimator.cs
+++ Assets/Scripts/TitlePieceAnimator.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TitlePieceAnimator : MonoBehaviour
 {
     [SerializeField] private GameObject m_Piece;
+    private Queue<(GameObject gameObject, float spawnedTime)> m_gameObjectAndSpawnedTime = new Queue<(GameObject gameObject, float spawnedTime)>();
 
     // Start is called before the first frame update
     void Start()
@@ -12,18 +13,28 @@ public class TitlePieceAnimator : MonoBehaviour
         StartCoroutine("DropPiece");
     }
 
+    private void Update()
+    {
+        if(m_gameObjectAndSpawnedTime.Peek().spawnedTime + 1.5f < Time.time)
+        {
+            (GameObject targetObject, float time) target = m_gameObjectAndSpawnedTime.Dequeue();
+            Destroy(target.targetObject);
+        }
+    }
+
     IEnumerator DropPiece()
     {
         while (true)
         {
-            float randomSpan = Random.Range(0.1f, 0.3f);
-
-            yield return new WaitForSeconds(randomSpan);
+            float randomSpan = Random.Range(0.1f, 0.5f);
 
             float x = Random.Range(1.5f, 4.5f);
-            var position = new Vector3(x, 3.5f, 5f);
+            var position = new Vector3(x, 4.5f, 5f);
             
-            Instantiate<GameObject>(m_Piece, position, Random.rotation, transform);
+            var spawnedPiece = Instantiate<GameObject>(m_Piece, position, Random.rotation, transform);
+            m_gameObjectAndSpawnedTime.Enqueue((spawnedPiece, Time.time));
+
+            yield return new WaitForSeconds(randomSpan);
         }
     }
 }
