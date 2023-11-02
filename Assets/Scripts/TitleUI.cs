bfa1e470faa441d14e86602b19156e9d98c25439
saikiRA <sumou1011.2@gmail.com>
2023-11-02 23:52:28 +0900

    comment: コメント追記

--- Assets/Scripts/TitleUI.cs
+++ Assets/Scripts/TitleUI.cs
@@ -12,6 +12,7 @@ public class TitleUI : MonoBehaviour
 
     public void ExitGame()
     {
+        // エディタ上で動作させる時とビルド時に動作させる時で停止命令が異なる
         #if UNITY_EDITOR
             UnityEditor.EditorApplication.isPlaying = false;
         #else
