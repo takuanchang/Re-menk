24a5cdb0f215d0e669af135b69ccc0b2178389ce
saikiRA <sumou1011.2@gmail.com>
2023-11-02 23:21:08 +0900

    comment: コメントを追加 and TitlePiece.csもういらないので削除

--- Assets/Scripts/TitlePieceAnimator.cs
+++ Assets/Scripts/TitlePieceAnimator.cs
@@ -15,7 +15,7 @@ public class TitlePieceAnimator : MonoBehaviour
 
     private void Update()
     {
-        if(m_gameObjectAndSpawnedTime.Peek().spawnedTime + 1.5f < Time.time)
+        if(m_gameObjectAndSpawnedTime.Peek().spawnedTime + 1.5f < Time.time) // キューの先頭にあるコマが発生から1.5秒経っていれば削除する
         {
             (GameObject targetObject, float time) target = m_gameObjectAndSpawnedTime.Dequeue();
             Destroy(target.targetObject);
@@ -26,13 +26,13 @@ public class TitlePieceAnimator : MonoBehaviour
     {
         while (true)
         {
-            float randomSpan = Random.Range(0.1f, 0.5f);
+            float randomSpan = Random.Range(0.1f, 0.5f); // コマの発生間隔はランダムな方が見た目良さそう。0.1秒～0.5秒くらいがうるさすぎず静かすぎずかな
 
-            float x = Random.Range(1.5f, 4.5f);
-            var position = new Vector3(x, 4.5f, 5f);
+            float x = Random.Range(1.5f, 4.5f); // メニューの右側にランダムな位置に落とす
+            var position = new Vector3(x, 4.5f, 5f); // コマの生成時にコライダーに押されて挙動が変わるのでちょっと上の方から落とす
             
-            var spawnedPiece = Instantiate<GameObject>(m_Piece, position, Random.rotation, transform);
-            m_gameObjectAndSpawnedTime.Enqueue((spawnedPiece, Time.time));
+            var spawnedPiece = Instantiate<GameObject>(m_Piece, position, Random.rotation, transform); 
+            m_gameObjectAndSpawnedTime.Enqueue((spawnedPiece, Time.time)); // キューにコマのGameObjectと発生時刻を保存しておく。
 
             yield return new WaitForSeconds(randomSpan);
         }
