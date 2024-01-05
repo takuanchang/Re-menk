5f6642e599513b27fdfdac40e3b77f4dee041191
saikiRA <sumou1011.2@gmail.com>
2024-01-05 23:46:30 +0900

    wip : オブジェクトの生成・受け渡しとアクセスのタイミングを考える必要あり

--- Assets/Scripts/PlayerGenerator.cs
+++ Assets/Scripts/PlayerGenerator.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class PlayerGenerator : MonoBehaviour
 {
     SettingManager setting;
 
-    [SerializeField] private HumanPlayer humanPrefab;
-    [SerializeField] private ComputerPlayer computerPrefab;
+    [SerializeField] private HumanPlayer m_HumanPrefab;
+    [SerializeField] private ComputerPlayer m_ComputerPrefab;
+    [SerializeField] private Camera m_MainCameraPrefab;
+    [SerializeField] private GameObject m_SelectCameraPrefab;
+    [SerializeField] private GameObject m_PieceCameraPrefab;
+    [SerializeField] private GameObject m_FreeLookCameraPrefab;
+    [SerializeField] private GameObject m_DollyCameraPrefab;
+
+    [SerializeField] private TurnManager m_TurnManager;
+    [SerializeField] private UiPrinter m_UiPrinter;
 
     void Start()
     {
@@ -18,55 +27,60 @@ public class PlayerGenerator : MonoBehaviour
         GameObject piecesCollector = GameObject.Find("PiecesCollerctor");
         Transform board = GameObject.Find("Board").transform;
 
-        Camera mainCameraOrigin = new Camera();
-        mainCameraOrigin.tag = "MainCamera";
-        mainCameraOrigin.cullingMask = (1 << 8 - 1); // プレイヤー系レイヤー以外をすべてONにする
-
-        Cinemachine.CinemachineFreeLook freeLookOrigin = new Cinemachine.CinemachineFreeLook();
-        freeLookOrigin.m_Orbits[0] = new Cinemachine.CinemachineFreeLook.Orbit(14, 5);
-        freeLookOrigin.m_Orbits[1] = new Cinemachine.CinemachineFreeLook.Orbit(9, 9);
-        freeLookOrigin.m_Orbits[2] = new Cinemachine.CinemachineFreeLook.Orbit(3, 10);
-        freeLookOrigin.Follow = board;
-        freeLookOrigin.LookAt = board;
-
-        Cinemachine.CinemachineVirtualCamera pieceCameraOrigin = new Cinemachine.CinemachineVirtualCamera();
-
         int freeLookPriority = 8; // この実装おかしい。現状は1番はじめに生成されたプレイヤーが遊ぶようになっている。
                                   // 誰から開始か、どのタイミングで決める？
 
         for (int i = 0; i < humanNum; i++)
         {
-            HumanPlayer humanPlayer = Instantiate(humanPrefab);
+            var myLayer = LayerMask.NameToLayer($"Player{i + 1}");
 
-            Camera mainCamera =  Instantiate(mainCameraOrigin);
-            mainCamera.cullingMask |= (1 << (8 + i));
+            HumanPlayer humanPlayer = Instantiate(m_HumanPrefab);
 
-            Cinemachine.CinemachineFreeLook freeLook = Instantiate(freeLookOrigin);
-            freeLook.Priority = freeLookPriority;
+            Camera mainCamera =  Instantiate(m_MainCameraPrefab);
+            mainCamera.cullingMask |= (1 << (myLayer));
 
-            Cinemachine.CinemachineVirtualCamera pieceCamera = Instantiate(pieceCameraOrigin);
+            GameObject freeLook = Instantiate(m_FreeLookCameraPrefab);
+            freeLook.layer = myLayer;
+            Cinemachine.CinemachineVirtualCameraBase freeLookBase = freeLook.GetComponent<Cinemachine.CinemachineVirtualCameraBase>();
+            freeLookBase.Priority = freeLookPriority;
 
-            humanPlayer.SetupCameras(mainCamera, freeLook, pieceCamera);
+            GameObject pieceCamera = Instantiate(m_PieceCameraPrefab);
+            freeLook.layer = myLayer;
+            Cinemachine.CinemachineVirtualCamera pieceCameraBase = pieceCamera.GetComponent<Cinemachine.CinemachineVirtualCamera>();
+            Assert.IsNotNull(pieceCameraBase);
+            pieceCameraBase.Follow = board;
+            pieceCameraBase.LookAt = board;
+
+            humanPlayer.SetupCameras(mainCamera, freeLookBase, pieceCameraBase);
 
             freeLookPriority = 11;
         }
 
-        for (int i = 0; i < cpuNum; i++)
+        for (int i = humanNum; i < cpuNum + humanNum; i++)
         {
-            ComputerPlayer computerPlayer = Instantiate(computerPrefab);
+            var myLayer = LayerMask.NameToLayer($"Player{i + 1}");
 
-            Camera mainCamera = Instantiate(mainCameraOrigin);
-            mainCamera.cullingMask |= (1 << (8 + humanNum + i));
+            ComputerPlayer computerPlayer = Instantiate(m_ComputerPrefab);
+
+            Camera mainCamera = Instantiate(m_MainCameraPrefab);
+            mainCamera.cullingMask |= (1 << myLayer);
 
             // これ本当はDolbyのカメラにした方がいい
-            Cinemachine.CinemachineFreeLook freeLook = Instantiate(freeLookOrigin);
-            freeLook.Priority = freeLookPriority;
+            GameObject freeLook = Instantiate(m_FreeLookCameraPrefab);
+            freeLook.layer = myLayer;
+            Cinemachine.CinemachineVirtualCameraBase freeLookBase = freeLook.GetComponent<Cinemachine.CinemachineVirtualCameraBase>();
+            freeLookBase.Priority = freeLookPriority;
 
-            Cinemachine.CinemachineVirtualCamera pieceCamera = Instantiate(pieceCameraOrigin);
+            GameObject pieceCamera = Instantiate(m_PieceCameraPrefab);
+            pieceCamera.layer = myLayer;
+            Cinemachine.CinemachineVirtualCamera pieceCameraBase = pieceCamera.GetComponent<Cinemachine.CinemachineVirtualCamera>();
+            pieceCameraBase.Follow = board;
+            pieceCameraBase.LookAt = board;
 
-            computerPlayer.SetupCameras(mainCamera, freeLook, pieceCamera);
+            computerPlayer.SetupCameras(mainCamera, freeLookBase, pieceCameraBase);
         }
 
+        m_TurnManager.SendMessage("");
     }
 
     // Update is called once per frame
