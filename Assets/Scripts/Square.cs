cb4506e8097084feddf1c8e9ede775ae886674eb
DESKTOP-S3T27CJ\takua <takuanoishiiyona@outlook.jp>
2024-01-27 22:30:24 +0900

    マス選択時にマスを光らせる

--- Assets/Scripts/Square.cs
+++ Assets/Scripts/Square.cs
@@ -6,13 +6,26 @@ public class Square : MonoBehaviour
 {
     private static readonly float VelocityThreshold = 5.0f;
 
-    /*
-    // Start is called before the first frame update
-    void Start()
+    private Material m_material;
+
+    private float m_HitPoint;
+    private const float initial_HP = 1.0f;
+
+    public void Initialize()
     {
-        //ToDo
+        m_material = GetComponent<Renderer>().material;
+        m_material.EnableKeyword("_EMISSION");
+        m_material.EnableKeyword("_EmissionColor");
+
+        m_HitPoint = initial_HP;
     }
 
+    void Start()
+    {
+        Initialize();
+    }
+
+    /*
     // Update is called once per frame
     void Update()
     {
@@ -23,11 +36,11 @@ public class Square : MonoBehaviour
     // TODO:要調整
     public void TurnOn()
     {
-        GetComponent<Renderer>().material.color = Color.yellow;
+        m_material.SetColor("_EmissionColor", Color.red);
     }
     public void TurnOff()
     {
-        GetComponent<Renderer>().material.color = Color.white;
+        m_material.SetColor("_EmissionColor", Color.black);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -37,6 +50,7 @@ public class Square : MonoBehaviour
             if (collision.relativeVelocity.magnitude > VelocityThreshold)
             {
                 p.Explode();
+
             }
         }
     }
